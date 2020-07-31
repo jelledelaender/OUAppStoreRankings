65a606b3a8e92701676d7c35577f49aa446fac9f
Jelle De Laender <jelle@codingmammoth.com>
2020-07-31 09:18:40 +0200

    Adding ruby ARGV to start script, based on # days

--- rankings.rb
+++ rankings.rb
@@ -3,21 +3,33 @@ load "dataprocessor.rb"
 require "sqlite3"
 
 def main
-  ## FROM ARGS...
-  start_date_input = "2020-07-25"
-  end_date_input = "2020-07-26"
+  ## Default values
+  number_of_days = 5;
+  db_location = "OURanking.sqlite"
 
-  begin
-    start_date = Date.parse start_date_input
-    end_date = Date.parse end_date_input
-  rescue Exception => e
-   puts "Exception: #{e}"
-   exit
+  ## Loading data from arguments
+  arg_length = ARGV.length
+  if arg_length < 1 || arg_length > 2
+    puts "Incorrect usage."
+    puts "Usage: ruby rankings.rb <number_of_days> <optional: DB Location>"
+    exit
   end
 
-  if end_date < start_date
-    puts "EndDate is before startDate"
-    exit
+  number_of_days = ARGV[0].to_i
+  db_location = ARGV[1] if arg_length > 1
+
+  ## Valiating inputs
+  end_date = Date.today.prev_day
+  start_date = end_date.prev_day(number_of_days - 1) ##Distracting one day as we'll still do the last day
+
+  if number_of_days < 1
+    puts "Minimal 1 day timespan. Setting to 1 day timespan"
+    start_date = end_date
+  end
+
+  if number_of_days > 90
+    puts "Maximal 90 days supported at the moment. Settin to 90 days timespan"
+    start_date = end_date.prev_day(90)
   end
 
   ## Start
@@ -25,11 +37,10 @@ def main
   puts "EndDate: #{end_date}"
   puts "-------------"
 
-  date = start_date
-
-  db = SQLite3::Database.new "OURanking.sqlite"
+  db = SQLite3::Database.new db_location
   db.execute "delete from apps" ## Debugging - emptying data set
 
+  date = start_date
   while date <= end_date
     dataProcessor = DataProcessor.new(db, date)
     dataProcessor.process
