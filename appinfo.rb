2e1751dfc1664e3cddbb65be3610afa84d4453a1
Jelle De Laender <jelle@codingmammoth.com>
2020-08-02 17:05:23 +0200

    Adding appinfo to get info of app

---
+++ appinfo.rb
@@ -0,0 +1,34 @@
+require "sqlite3"
+load "lib/pretty_table.rb"
+
+def main
+  db_location = "OURanking.sqlite"
+
+  ## Loading data from arguments
+  arg_length = ARGV.length
+  if arg_length < 1 || arg_length > 2
+    puts "Incorrect usage."
+    puts "Usage: ruby toplist.rb <appid> <optional:DB Location>"
+    exit
+  end
+
+  ## AppID
+  app_id = ARGV[0]
+
+  ## Alternative DB_Location
+  db_location = ARGV[2] if arg_length > 2
+
+  ## App info
+  db = SQLite3::Database.new db_location
+  db.results_as_hash = true
+
+  result = db.query( "SELECT name, count(*) as times_seen_in_database, * FROM apps WHERE app_id = ? LIMIT 1", app_id)
+
+  result.each do |u|
+    u.keys.each do |key|
+      puts "#{key}: #{u[key]}"
+    end
+  end
+end
+
+main
